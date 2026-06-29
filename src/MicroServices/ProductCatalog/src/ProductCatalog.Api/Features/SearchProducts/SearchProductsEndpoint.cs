8918d35b0f1ef67a396c566f5356ee519a03a341
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-29 17:05:49 +0200

    Dodanie wyszukiwania produktow

---
+++ src/MicroServices/ProductCatalog/src/ProductCatalog.Api/Features/SearchProducts/SearchProductsEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using ProductCatalog.Application.SearchProducts;
+
+namespace ProductCatalog.Api.Features.SearchProducts;
+
+public static class SearchProductsEndpoint
+{
+    // GET api/products?color=red&group=B
+    public static IEndpointRouteBuilder MapSearchProductsEndpoint(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("api/products", async ([AsParameters] SearchProductsRequest request, SarchProductsHandler handler) => await handler.HandleAsync(request) );
+
+        return endpoints;
+    }
+}
+
+
