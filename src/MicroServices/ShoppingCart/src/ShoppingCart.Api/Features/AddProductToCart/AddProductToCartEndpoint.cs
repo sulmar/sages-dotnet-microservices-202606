ca2a9341fab312925c56d1d515cb8ac0abc9f5a2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-29 17:12:34 +0200

    Dodanie koszyka zakupowego

---
+++ src/MicroServices/ShoppingCart/src/ShoppingCart.Api/Features/AddProductToCart/AddProductToCartEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using ShoppingCart.Application.Features.AddProductToCart;
+
+namespace ShoppingCart.Api.Features.AddProductToCart;
+
+public static class AddProductToCartEndpoint
+{
+    public static IEndpointRouteBuilder MapAddProductToCartEndpoint(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("api/cart", (AddProductToCartHandler  handler) => Results.Ok());
+
+        return endpoints;
+    }
+}
---
+++ src/MicroServices/ShoppingCart/src/ShoppingCart.Application/Features/AddProductToCart/AddProductToCartHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Application.Features.AddProductToCart
+{
+    internal class AddProductToCartHandler
+    {
+    }
+}
