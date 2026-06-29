85e28ed46a65d7eab88fc33c6263eef6285fd3e0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-29 17:05:49 +0200

    Dodanie wyszukiwania produktow

---
+++ src/MicroServices/ProductCatalog/src/ProductCatalog.Application/SearchProducts/SarchProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿using ProductCatalog.Application.Features.GetProducts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductCatalog.Application.SearchProducts;
+
+public class SarchProductsHandler
+{
+    public async Task<IReadOnlyList<ProductListItemDto>> HandleAsync(SearchProductsRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+public record SearchProductsRequest(string Color, string Group);
