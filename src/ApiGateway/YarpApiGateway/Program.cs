d3cc60a94f76e61c65a7a63c73d4e193e722ef4a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-06-30 15:04:47 +0200

    Dodanie auto discovery do bramki YARP

--- src/ApiGateway/YarpApiGateway/Program.cs
+++ src/ApiGateway/YarpApiGateway/Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// dotnet add package Microsoft.Extensions.ServiceDiscovery
+builder.Services.AddServiceDiscovery();
+
 // dotnet add package Yarp.ReverseProxy
+// dotnet add package Microsoft.Extensions.ServiceDiscovery.Yarp
+
 builder.Services.AddReverseProxy()    
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddServiceDiscoveryDestinationResolver();
 
 var app = builder.Build();
 
--- src/MicroServices/ProductCatalog/src/ProductCatalog.Api/Program.cs
+++ src/MicroServices/ProductCatalog/src/ProductCatalog.Api/Program.cs
@@ -4,22 +4,24 @@ using ProductCatalog.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Read", policy =>
-    {
-        policy.AllowAnyOrigin();
-        policy.WithMethods("GET");
-        policy.AllowAnyHeader();        
-    });
-});
-
 builder.Services.AddAplication();
 builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
-// app.UseCors("Read");
+app.Use(async (context, next) =>
+{
+    context.Response.OnStarting(() =>
+    {
+        context.Response.Headers["X-Server"] = Environment.MachineName;
+        context.Response.Headers["X-Server-Port"] =
+            context.Connection.LocalPort.ToString();
+
+        return Task.CompletedTask;
+    });
+
+    await next();
+});
 
 app.MapGet("/", () => "Hello Product Catalog Api!");
 app.MapGetProducts();
