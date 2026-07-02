6eb8491278f3841f3aba549b343be93f72f0e680
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-07-02 15:02:03 +0200

    Dodanie uslugi do generowania tokenu jwt

---
+++ src/IdentityProvider/IdentityProvider.Api/Features/Login/LoginEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace IdentityProvider.Api.Features.Login;
+
+public static class LoginEndpoint
+{
+    public static IEndpointRouteBuilder MapLogin(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("api/login", async (LoginRequest request, LoginHandler handler) =>
+        {
+            var response = await handler.HandleAsync(request);
+
+            if (response.IsSuccess)
+            {
+                return Results.Ok(response.AccessToken);
+            }
+            else
+            {
+                return Results.Unauthorized();
+            }
+        });
+
+        return endpoints;
+    }
+}
---
+++ src/IdentityProvider/IdentityProvider.Api/Infrastructure/JwtTokenGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityProvider.Api.Infrastructure;
+
+public class JwtTokenGenerator
+{
+
+    // dotnet add package Microsoft.IdentityModel.JsonWebTokens
+
+    public string Generate(string userId, string username)
+    {
+        var expiresAt = DateTime.UtcNow.AddMinutes(5);
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("a-string-secret-at-least-256-bits-long"));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new Dictionary<string, object>
+        {
+            ["sub"] = userId,
+            ["name"] = username,
+            ["exp"] = expiresAt.ToString("o")
+        };
+
+        var descriptor = new SecurityTokenDescriptor
+        {
+            Issuer = "a",
+            Audience = "b",
+            Claims = claims,
+            Expires = expiresAt,
+            SigningCredentials = credentials
+        };
+
+        var tokenHandler = new JsonWebTokenHandler();
+
+        return tokenHandler.CreateToken(descriptor);
+    }
+}
